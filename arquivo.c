92ceeb1e1617f671a7665b403fc6eff95cba9326
Rafael O. Freire <120592511+rfreir3@users.noreply.github.com>
2023-03-02 15:38:50 -0300

    Create arquivo.c

---
+++ arquivo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+	int c;
+	char entrada[121];
+	char saida[121];
+	FILE *e;
+	FILE *s;
+	printf("Digite o nome do arquivo de entrada: ");
+	scanf("%120s", entrada);
+	printf("Digite o nome do arquivo de saida: ");
+	scanf("%120s", saida);
+	e = fopen(entrada, "rt");
+	s = fopen(saida, "wt");
+	if(e == NULL){
+		printf("Erro! Arquivo de entrada\n");
+		return(1);
+	}
+	else if(s == NULL){
+		printf("Erro! Arquivo de saída\n");
+		fclose(e);
+		return(1);
+	}
+	while((c=fgetc(e))!= EOF){
+		fputc(toupper(c), s);
+	}
+	fclose(e);
+	fclose(s);
+	return(0);
+}
